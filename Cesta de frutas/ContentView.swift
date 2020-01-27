166747ad5c0b8d483e00ea6ed02036f53e325413
Joel Junior <joel.asj2@gmail.com>
2020-01-27 11:06:00 -0300

    Initial Commit

---
+++ Cesta de frutas/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cesta de frutas
+//
+//  Created by Joel Júnior on 27/01/20.
+//  Copyright © 2020 jnr. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
